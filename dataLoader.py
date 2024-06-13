be584c28b621c7f57b7425a3ac6fa9f666707559
Zhangxi <zhangxi_my@sina.cn>
2024-06-13 22:09:26 +0800

    增加数据增强

--- dataLoader.py
+++ dataLoader.py
@@ -1,3 +1,4 @@
+import cv2
 import torch
 from torch.utils.data.dataset import Dataset
 from PIL import Image
@@ -32,7 +33,7 @@ class YoloDataset(Dataset):
 
         return image, boxes
 
-    def get_item_data(self, index):
+    def get_item_data(self, index, jitter=.3 ,hue=.1, sat=0.7, val=0.4, random=True):
         item = self.lines[index].split()
         image = Image.open(item[0])
         image = cvtColor(image)
@@ -40,37 +41,98 @@ class YoloDataset(Dataset):
         iw, ih = image.size
         w, h = self.input_shape  # 416 * 416
 
-        scale = min(w / iw, h / ih)
-        nw = int(iw * scale)
-        nh = int(ih * scale)
-        dx = (w - nw) // 2
-        dy = (h - nh) // 2
-
-        # print("缩放比".format(scale))
-        # print("原始图的寬高={},{}".format(iw,ih))
-        # print("缩放后寬高={},{}".format(nw,nh))
-
-        # 将原始图像进行缩放符合416*416 的输入特征
-        image = image.resize((nw, nh), Image.BICUBIC)
-        new_image = Image.new('RGB', (w, h), (128, 128, 128))
-        new_image.paste(image, (dx, dy))
-        image_data = np.array(new_image, np.float32)
-
         # 计算缩放后的bnd box
         box = np.array([np.array(list(map(int, box.split(',')))) for box in item[1:]])
 
-        # bndbox 进行缩放后其他区域的填充操作 对应的 ground truth 框也需要进行dx dy 的偏移
+        if not random:
+            scale = min(w / iw, h / ih)
+            nw = int(iw * scale)
+            nh = int(ih * scale)
+            dx = (w - nw) // 2
+            dy = (h - nh) // 2
+
+            # 将原始图像进行缩放符合416*416 的输入特征
+            image = image.resize((nw, nh), Image.BICUBIC)
+            new_image = Image.new('RGB', (w, h), (128, 128, 128))
+            new_image.paste(image, (dx, dy))
+            image_data = np.array(new_image, np.float32)
+
+            # bndbox 进行缩放后其他区域的填充操作 对应的 ground truth 框也需要进行dx dy 的偏移
+            if len(box) > 0:
+                np.random.shuffle(box)
+                box[:, [0, 2]] = box[:, [0, 2]] * nw/iw + dx
+                box[:, [1, 3]] = box[:, [1, 3]] * nh/ih + dy
+                box[:, 0:2][box[:, 0:2] < 0] = 0
+                box[:, 2][box[:, 2] > w] = w
+                box[:, 3][box[:, 3] > h] = h
+                box_w = box[:, 2] - box[:, 0]
+                box_h = box[:, 3] - box[:, 1]
+                box = box[np.logical_and(box_w > 1, box_h > 1)]  # discard invalid box
+            return image_data, box
+
+        # 对图像进行随机缩放并且进行长款的扭曲
+        new_ar = iw/ih * (self.rand(1-jitter,1+jitter) / self.rand(1-jitter,1+jitter))
+        scale = self.rand(0.25, 2)
+        if new_ar < 1:
+            nh = int(scale * h)
+            nw = int(nh * new_ar)
+        else:
+            nw = int(scale * w)
+            nh = int(nw / new_ar)
+
+        image = image.resize((nw,nh), Image.BICUBIC)
+
+        # ------------------------------------------#
+        #   将图像多余的部分加上灰条
+        # ------------------------------------------#
+        dx = int(self.rand(0, w - nw))
+        dy = int(self.rand(0, h - nh))
+        new_image = Image.new('RGB', (w, h), (128, 128, 128))
+        new_image.paste(image, (dx, dy))
+        image = new_image
+
+        # 图像左右翻转
+        flip = self.rand() < .5
+        if flip:
+            image = image.transpose(Image.FLIP_LEFT_RIGHT)
+
+        image_data = np.array(image, np.uint8)
+
+        r = np.random.uniform(-1, 1, 3) * [hue, sat, val] + 1
+        hue, sat, val = cv2.split(cv2.cvtColor(image_data, cv2.COLOR_RGB2HSV))
+        dtype = image_data.dtype
+
+        # ---------------------------------#
+        #   应用变换
+        # ---------------------------------#
+        x = np.arange(0, 256, dtype=r.dtype)
+        lut_hue = ((x * r[0]) % 180).astype(dtype)
+        lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
+        lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
+
+        image_data = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val)))
+        image_data = cv2.cvtColor(image_data, cv2.COLOR_HSV2RGB)
+
         if len(box) > 0:
+            np.random.shuffle(box)
             box[:, [0, 2]] = box[:, [0, 2]] * nw/iw + dx
             box[:, [1, 3]] = box[:, [1, 3]] * nh/ih + dy
+            if flip:
+                box[:, [0, 2]] = w - box[:, [2, 0]] # ?? box[:, [0, 2]] = w - box[:, [0, 2]]
             box[:, 0:2][box[:, 0:2] < 0] = 0
             box[:, 2][box[:, 2] > w] = w
             box[:, 3][box[:, 3] > h] = h
+
             box_w = box[:, 2] - box[:, 0]
             box_h = box[:, 3] - box[:, 1]
-            box = box[np.logical_and(box_w > 1, box_h > 1)]  # discard invalid box
+
+            box = box[np.logical_and(box_w > 1, box_h > 1)]
+
         return image_data, box
 
+
+    def rand(self, a=0,b=1):
+        return np.random.rand() * (b - a) + a
 
 def yolo_dataset_collate(batch):
     # 方法1
